f1baacc68fe7158f3cf3e523ea9a28752cd34b27
vetrivel-muthusamy <vetrivel.muthusamy01@gmail.com>
2021-12-13 18:54:36 +0530

    WebDriver Tutorial 11 - Understand Webdriver Interface

--- src/test/java/ui/logintest.java
+++ src/test/java/ui/logintest.java
@@ -1,24 +1,42 @@
 package ui;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class logintest {
-
+	
+	public static String browser = "Firefox"; //Assuming this input getting from external File like XLS, CSV (Configuration)
+	public static WebDriver driver;
+	
 	public static void main(String[] args) {
 		
-		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+		if(browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("Chrome")) {
+			WebDriverManager.chromiumdriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equals("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+				
+		driver.get("http://automationpractice.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a")).click();
+		driver.findElement(By.id("email")).sendKeys("abc1234@xyz.com");
+		driver.findElement(By.id("passwd")).sendKeys("Test@123");
+		driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span/i")).click();
+		driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[1]/ul/li[4]/a/span")).click();
+		driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[2]/a")).click();
 		
-		driver.get("https://www.saucedemo.com/");
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-		//Xpath to be used - personal note
-		driver.findElement(By.id("login-button")).click();
 		
-		driver.close();
+		driver.quit();
 	}
 
 }
